ef0875339d925f46c2b59b88c1c6f638a3b739b3
BaBa7080 <abhishekshukla0700@gmail.com>
2024-04-12 14:30:30 +0530

    MVC created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+class UserController extends Controller
+{
+    //
+    public function show(string $id): View
+    {
+        // return view('user.profile', [
+        //     'user' => User::findOrFail($id)
+        // ]);
+        return view('welcome');
+    }
+}
